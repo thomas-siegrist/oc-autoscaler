33f416c6cd5bce67852b9fd1d1002eddef0e1d43
Thomas Siegrist <thomas@carmens-macbook-pro.home>
2016-08-20 11:36:54 +0200

    Initial Setup of Components

---
+++ metrics-interpreter/src/main/java/ch/sbb/cloud/autoscaler/MetricsInterpreterApplication.java
@@ -0,0 +1,12 @@
+package ch.sbb.cloud.autoscaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsInterpreterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsInterpreterApplication.class, args);
+	}
+}
---
+++ scaler/src/main/java/ch/sbb/cloud/autoscaler/ScalerApplication.java
@@ -0,0 +1,12 @@
+package ch.sbb.cloud.autoscaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalerApplication.class, args);
+	}
+}
