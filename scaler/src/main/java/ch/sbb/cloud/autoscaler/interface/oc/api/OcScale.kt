1ce8c6dafb6800c1b68408db54ce36f349d19255
Thomas Siegrist <thomas@carmens-macbook-pro.home>
2016-08-27 14:08:18 +0200

    zwischencommit: Scaler-Service

---
+++ scaler/src/main/java/ch/sbb/cloud/autoscaler/interface/oc/api/OcScale.kt
@@ -0,0 +1,12 @@
+package ch.sbb.cloud.autoscaler.`interface`.oc.api
+
+/**
+ * Created by thomas on 25.08.16.
+ */
+class OcScale(val spec: OcSpec) {
+
+    class OcSpec(replicas: Int) {
+
+    }
+
+}
