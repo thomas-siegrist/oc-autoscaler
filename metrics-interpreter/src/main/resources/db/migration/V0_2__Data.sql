4787ac56e87858f395fd33f146adaac749776dc0
siegrist <sigibe@msn.com>
2016-09-11 21:38:02 +0200

    Event-Names fixed

--- metrics-interpreter/src/main/resources/db/migration/V0_2__Data.sql
+++ metrics-interpreter/src/main/resources/db/migration/V0_2__Data.sql
@@ -1,4 +1,4 @@
 INSERT INTO configuration (id, project, service, metrics, metric_name, scale_up, scale_down)
-VALUES (0, 'usecase', 'emailservice', 'QUEUE_DEPTH', 'email-queue-depth', 15, 0);
+VALUES (0, 'usecase', 'emailservice', 'QUEUE_DEPTH', 'email-queue', 10, 0);
 INSERT INTO configuration (id, project, service, metrics, metric_name, scale_up, scale_down)
-VALUES (1, 'usecase', 'printservice', 'QUEUE_DEPTH', 'print-queue-depth', 15, 0);
+VALUES (1, 'usecase', 'printservice', 'QUEUE_DEPTH', 'print-queue', 10, 0);
