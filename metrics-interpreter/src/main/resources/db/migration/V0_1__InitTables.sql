2edf4e06d12438d2720012e0d724b3612f1aa113
Thomas Siegrist <thomas@carmens-macbook-pro.home>
2016-09-12 17:53:11 +0200

    Reorganized Init-Scripts and added new Start-Value: 10'000 to prevent collisions

--- metrics-interpreter/src/main/resources/db/migration/V0_1__InitTables.sql
+++ metrics-interpreter/src/main/resources/db/migration/V0_1__InitTables.sql
@@ -10,3 +10,14 @@ CREATE TABLE configuration
   CONSTRAINT configuration_pkey PRIMARY KEY (id),
   CONSTRAINT domainkey UNIQUE (project, service, metrics, metric_name)
 );
+
+CREATE TABLE service_limit
+(
+  id bigint NOT NULL,
+  project character varying(255) NOT NULL,
+  service character varying(255) NOT NULL,
+  min_pods bigint NOT NULL,
+  max_pods bigint NOT NULL,
+  CONSTRAINT servicelimit_pkey PRIMARY KEY (id),
+  CONSTRAINT servicelimit_domainkey UNIQUE (project, service)
+);
