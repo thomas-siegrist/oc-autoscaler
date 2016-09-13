78fa73552bee74eb85eba559260b4e690a8076b1
Mico Micic <mico@micic.ch>
2016-09-13 23:42:11 +0200

    Implemented statistics API. Will be used for the statistics "live view".

--- autoscale-interpreter/src/main/java/ch/sbb/cloud/autoscaler/service/MetricsInterpreterService.java
+++ autoscale-interpreter/src/main/java/ch/sbb/cloud/autoscaler/service/MetricsInterpreterService.java
@@ -5,23 +5,19 @@ import ch.sbb.cloud.autoscaler.model.MetricsEvent;
 import ch.sbb.cloud.autoscaler.model.ServiceLimit;
 import ch.sbb.cloud.autoscaler.model.actionevents.ActionEvent;
 import ch.sbb.cloud.autoscaler.model.actionevents.Scale;
+import ch.sbb.cloud.autoscaler.model.stats.MetricsStatistic;
 import ch.sbb.cloud.autoscaler.repository.ConfigurationRepository;
 import ch.sbb.cloud.autoscaler.repository.ServiceLimitRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hazelcast.config.MultiMapConfig;
-import com.hazelcast.core.HazelcastInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * Created by thomas on 01.09.16.
@@ -35,15 +31,6 @@ public class MetricsInterpreterService {
 
     private static final long SCALE_DOWN_DELAY_IN_SECONDS = 60;
 
-    private static final String HZ_METRICS_MAP = "Metrics";
-    private static final String HZ_LAST_SCALE_MAP = "Last-Scale";
-
-    @Autowired
-    private HazelcastInstance hz;
-
-    @Autowired
-    private RabbitTemplate amqp;
-
     @Autowired
     private RabbitTemplate template;
 
@@ -53,39 +40,38 @@ public class MetricsInterpreterService {
     @Autowired
     private ServiceLimitRepository serviceLimitRepository;
 
-    @PostConstruct
-    public void init() {
-        MultiMapConfig multiMapConfig = new MultiMapConfig();
-        multiMapConfig
-                .setName(HZ_METRICS_MAP)
-                .setBackupCount(0)
-                .setAsyncBackupCount(1)
-                .setValueCollectionType(MultiMapConfig.ValueCollectionType.LIST);
-        hz.getConfig().addMultiMapConfig(multiMapConfig);
-    }
+    @Autowired
+    private MetricsPersistenceService persistenceService;
 
     public void postNewEvent(MetricsEvent metricsEvent) {
-        persistInHazelcast(metricsEvent);
-        interpretEvent(metricsEvent);
+
+        // Calculate the overall value only once...
+        Long value = calculateValue(metricsEvent);
+
+        persistenceService.persistMetricsEvent(metricsEvent);
+        persistenceService.persistMetricsStatistic(fromEventToStat(metricsEvent, value));
+
+        interpretEvent(metricsEvent, value);
     }
 
-    private void persistInHazelcast(MetricsEvent metricsEvent) {
-        hz
-                .getMultiMap(HZ_METRICS_MAP)
-                .put(
-                        metricsEvent.composedUniqueId(),
-                        metricsEvent.getValue()
-                );
+    private MetricsStatistic fromEventToStat(MetricsEvent metricsEvent, Long currentMetricValue) {
+        MetricsStatistic stat = new MetricsStatistic();
+        stat.setProject(metricsEvent.getProject());
+        stat.setService(metricsEvent.getService());
+        stat.setMetricName(metricsEvent.getMetricName());
+        stat.setMetrics(metricsEvent.getMetrics());
+        stat.setCurrentValue(currentMetricValue);
+
+        return stat;
     }
 
-    private void interpretEvent(MetricsEvent metricsEvent) {
+    private void interpretEvent(MetricsEvent metricsEvent, Long value) {
         Configuration configuration = searchForMatchingConfiguration(metricsEvent);
         if (configuration == null) {
             LOG.warn("No metrics configuration found for {}", metricsEvent.composedUniqueId());
             return; // No Config => No Action
         }
 
-        Long value = calculateValue(metricsEvent);
         if (value >= configuration.getScaleUp()) {
             scaleUp(metricsEvent, configuration);
         } else if (value <= configuration.getScaleDown()) {
@@ -98,30 +84,16 @@ public class MetricsInterpreterService {
     private Long calculateValue(MetricsEvent metricsEvent) {
         Long value = metricsEvent.getValue();
         if (metricsEvent.getMetrics().isAggregate()) {
-            value = calculateAggregateFor(metricsEvent);
+            value = persistenceService.calculateAggregateFor(metricsEvent);
         }
         return value;
-    }
-
-    private Long calculateAggregateFor(MetricsEvent metricsEvent) {
-        List<Long> values = hz
-                .getMultiMap(HZ_METRICS_MAP)
-                .get(metricsEvent.composedUniqueId())
-                .stream()
-                .map(v -> (Long) v)
-                .collect(Collectors.toList());
-
-        return values.stream()
-                .skip(values.size() > 10 ? values.size() - 10 : 0)
-                .collect(Collectors.averagingLong(v -> v))
-                .longValue();
     }
 
     private void scaleUp(MetricsEvent metricsEvent, Configuration configuration) {
         if (isUpscalingAllowed(metricsEvent)) {
             LOG.info("Scaling up service {}", metricsEvent.getService());
             send(actionEventFor(configuration, Scale.UP));
-            setLastScaleTimeToNow(metricsEvent);
+            persistenceService.setLastScaleTimeToNow(metricsEvent);
         }
     }
 
@@ -129,35 +101,20 @@ public class MetricsInterpreterService {
         if (isDownscalingAllowed(metricsEvent)) {
             LOG.info("Scaling down service {}", metricsEvent.getService());
             send(actionEventFor(configuration, Scale.DOWN));
-            setLastScaleTimeToNow(metricsEvent);
+            persistenceService.setLastScaleTimeToNow(metricsEvent);
         }
     }
 
     private boolean isUpscalingAllowed(MetricsEvent metricsEvent) {
-        return getLastScaleDateTime(metricsEvent)
+        return persistenceService.getLastScaleDateTime(metricsEvent)
                 .orElse(LocalDateTime.MIN)
                 .isBefore(LocalDateTime.now().minusSeconds(SCALE_UP_DELAY_IN_SECONDS));
     }
 
     private boolean isDownscalingAllowed(MetricsEvent metricsEvent) {
-        return getLastScaleDateTime(metricsEvent)
+        return persistenceService.getLastScaleDateTime(metricsEvent)
                 .orElse(LocalDateTime.MIN)
                 .isBefore(LocalDateTime.now().minusSeconds(SCALE_DOWN_DELAY_IN_SECONDS));
-    }
-
-    private Optional<LocalDateTime> getLastScaleDateTime(MetricsEvent metricsEvent) {
-        return Optional.ofNullable((LocalDateTime) hz
-                .getMap(HZ_LAST_SCALE_MAP)
-                .get(metricsEvent.composedUniqueId()));
-    }
-
-    private void setLastScaleTimeToNow(MetricsEvent metricsEvent) {
-        hz
-                .getMap(HZ_LAST_SCALE_MAP)
-                .put(
-                        metricsEvent.composedUniqueId(),
-                        LocalDateTime.now()
-                );
     }
 
     private Configuration searchForMatchingConfiguration(MetricsEvent metricsEvent) {
---
+++ autoscale-interpreter/src/main/java/ch/sbb/cloud/autoscaler/service/MetricsPersistenceService.java
@@ -0,0 +1,103 @@
+package ch.sbb.cloud.autoscaler.service;
+
+import ch.sbb.cloud.autoscaler.model.MetricsEvent;
+import ch.sbb.cloud.autoscaler.model.stats.MetricsStatistic;
+import com.hazelcast.config.MultiMapConfig;
+import com.hazelcast.core.HazelcastInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
+
+/**
+ * Created by micic on 13.09.16.
+ */
+
+@Component
+public class MetricsPersistenceService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MetricsPersistenceService.class);
+
+    private static final String HZ_METRICS_MAP = "Metrics";
+    private static final String HZ_METRICS_STATS_MAP = "MetricsStats";
+    private static final String HZ_LAST_SCALE_MAP = "Last-Scale";
+
+    @Autowired
+    private HazelcastInstance hz;
+
+    @PostConstruct
+    public void init() {
+        MultiMapConfig multiMapConfig = new MultiMapConfig();
+        multiMapConfig
+                .setName(HZ_METRICS_MAP)
+                .setBackupCount(0)
+                .setAsyncBackupCount(1)
+                .setValueCollectionType(MultiMapConfig.ValueCollectionType.LIST);
+        hz.getConfig().addMultiMapConfig(multiMapConfig);
+    }
+
+    void persistMetricsStatistic(MetricsStatistic metricsStat) {
+        hz
+                .getMap(HZ_METRICS_STATS_MAP)
+                .put(
+                        metricsStat.composedUniqueId(),
+                        metricsStat
+                );
+    }
+
+    public List<MetricsStatistic> getAllStatistics() {
+        return hz.getMap(HZ_METRICS_STATS_MAP)
+                .values()
+                .stream()
+                .map(v -> (MetricsStatistic)v)
+                .collect(Collectors.toList());
+    }
+
+    void persistMetricsEvent(MetricsEvent metricsEvent) {
+        hz
+                .getMultiMap(HZ_METRICS_MAP)
+                .put(
+                        metricsEvent.composedUniqueId(),
+                        metricsEvent.getValue()
+                );
+    }
+
+    Long calculateAggregateFor(MetricsEvent metricsEvent) {
+        List<Long> values = hz
+                .getMultiMap(HZ_METRICS_MAP)
+                .get(metricsEvent.composedUniqueId())
+                .stream()
+                .map(v -> (Long) v)
+                .collect(Collectors.toList());
+
+        Double avg = values.stream()
+                .skip(values.size() > 10 ? values.size() - 10 : 0)
+                .mapToLong(v -> v)
+                .average()
+                .orElseGet(() -> 0);
+
+        return avg.longValue();
+    }
+
+    Optional<LocalDateTime> getLastScaleDateTime(MetricsEvent metricsEvent) {
+        return Optional.ofNullable((LocalDateTime) hz
+                .getMap(HZ_LAST_SCALE_MAP)
+                .get(metricsEvent.composedUniqueId()));
+    }
+
+    void setLastScaleTimeToNow(MetricsEvent metricsEvent) {
+        hz
+                .getMap(HZ_LAST_SCALE_MAP)
+                .put(
+                        metricsEvent.composedUniqueId(),
+                        LocalDateTime.now()
+                );
+    }
+}
